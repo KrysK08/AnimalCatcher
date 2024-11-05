0d82ab75b2dbd1f1a71f3f60806c05b80d6a790c
Zuzanna <zuzia.drozdz@wp.pl>
2024-11-05 12:44:04 +0100

    Dodałyśmy postacie i rozmieściłyśmy buttony na ekranie

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            MyJFrame window = new MyJFrame(); // Create the main window
+            MyJFrame window = new MyJFrame();
         });
     }
 }
--- src/main/java/org/example/window/MyJPanel.java
+++ src/main/java/org/example/window/MyJPanel.java
@@ -5,21 +5,46 @@ import java.awt.*;
 
 public class MyJPanel extends JPanel {
     public MyJPanel() {
-        setLayout(new FlowLayout()); // Set layout for the panel
-
-        JButton buttonTitle = new JButton("Wybierz postać");
-        JButton buttonBoy = new JButton("Boy");
-        JButton buttonGirl = new JButton("Girl");
-
-        buttonTitle.setPreferredSize(new Dimension(150, 50)); // Set button sizes
-        buttonBoy.setPreferredSize(new Dimension(150, 50));
-        buttonGirl.setPreferredSize(new Dimension(150, 50));
-
         setLayout(new BorderLayout());
 
+        JButton buttonTitle = new JButton("Wybierz postać");
+        buttonTitle.setPreferredSize(new Dimension(200, 60));
+        buttonTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        buttonTitle.setFont(new Font("Arial", Font.BOLD, 25));
+
+        ImageIcon buttonBImage = new ImageIcon("C:/Users/uczen/IdeaProjects/AnimalCatcher/src/main/java/org/example/img/boy.jpg");
+        Image imageboy = buttonBImage.getImage();
+        Image scaledImage = imageboy.getScaledInstance(320, 300, Image.SCALE_SMOOTH);
+        ImageIcon scaledButtonBImage = new ImageIcon(scaledImage);
+        JButton buttonBoyImage = new JButton(scaledButtonBImage);
+        buttonBoyImage.setPreferredSize(new Dimension(320, 300));
+
+        ImageIcon buttonGImage = new ImageIcon("C:/Users/uczen/IdeaProjects/AnimalCatcher/src/main/java/org/example/img/girl.jpg");
+        Image imagegirl = buttonGImage.getImage();
+        Image scaledGrilImage = imagegirl.getScaledInstance(300, 300, Image.SCALE_SMOOTH);
+        ImageIcon scaledButtonGImage = new ImageIcon(scaledGrilImage);
+        JButton buttonGirlImage = new JButton(scaledButtonGImage);
+        buttonGirlImage.setPreferredSize(new Dimension(300, 300));
+
+
+        JPanel imagePanel = new JPanel();
+        imagePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
+        imagePanel.add(buttonBoyImage);
+        imagePanel.add(buttonGirlImage);
+
+        JTextField boyStats = new JTextField("Statystyki", 20);
+        boyStats.setPreferredSize(new Dimension(200, 60));
+
+        JTextField girlStats = new JTextField("Statystyki", 20);
+        girlStats.setPreferredSize(new Dimension(200, 60));
+
+        JPanel statsPanel = new JPanel();
+        statsPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
+        statsPanel.add(boyStats);
+        statsPanel.add(girlStats);
+
         add(buttonTitle, BorderLayout.NORTH);
-        add(buttonBoy, BorderLayout.WEST);
-        add(buttonGirl, BorderLayout.EAST);
+        add(imagePanel, BorderLayout.CENTER);
 
         setVisible(true);
     }
